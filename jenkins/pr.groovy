b4ceeaed82b2451c45864c7cbb76f996b4ae7f78
ToniNichev <toni.nichev@gmail.com>
2020-04-21 15:07:18 -0400

    pipeline updates

--- jenkins/pr.groovy
+++ jenkins/pr.groovy
@@ -19,7 +19,7 @@ pipeline {
       }
     }
      
-    stage('Test') {
+    stage('Running Tests') {
       steps {
         echo '######################'              
         echo 'Running tests ...'          
