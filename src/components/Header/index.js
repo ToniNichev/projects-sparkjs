3543128ab311e4b71400f7302ed0d6a32af7055b
ToniNichev <toni.nichev@gmail.com>
2020-04-21 21:39:19 -0400

    pipeline updates

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,7 +10,7 @@ const Header = ( {title} ) => (
       <ul>
         <li><Link to='/home'>HOME</Link></li>
         <li><Link to='/greetings'>GREETINGS</Link></li>       
-        <li><Link to='/about'>ABOUT</Link></li>
+        <li><Link to='/about'>ABOUT (1)</Link></li>
       </ul>
     </div>
   </div>
