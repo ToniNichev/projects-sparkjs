f9d4ad11a1e4d3deb3d7049cf2c9639511d91fb8
ToniNichev <toni.nichev@gmail.com>
2020-04-23 22:26:50 -0400

    Disabling CRF

--- jenkins/pr.groovy
+++ jenkins/pr.groovy
@@ -3,3 +3,42 @@ import jenkins.model.Jenkins
 def instance = Jenkins.instance
 instance.setCrumbIssuer(null)
 
+pipeline {
+  agent any
+    
+  tools {nodejs "SparkJS"}
+    
+  stages {
+        
+    stage('Cloning Git Repo') {
+      steps {
+        git 'https://github.com/ToniNichev/projects-sparkjs.git'
+      }
+    }
+    stage('Install dependencies') {
+      steps {
+        echo '######################'              
+        echo 'Building...'       
+        echo '######################'                      
+        sh '/usr/local/bin/yarn install'
+      }
+    }
+     
+    stage('Running Tests') {
+      steps {
+        echo '######################'              
+        echo 'Running tests ...'          
+        echo '######################'               
+         sh '/usr/local/bin/yarn test'
+      }
+    }      
+  }
+
+  post { 
+      always { 
+          echo 'Starting server ...'
+          sh '/usr/local/bin/yarn clean; /usr/local/bin/yarn build-prod; /usr/local/bin/yarn build-prod-ssr;'
+          sh '/usr/local/bin/pm2 start ./server-build/server-bundle.js -f'
+      }
+  }  
+}
