e2f125db9866fbfd233bc42e8395d636fbb1ac64
ToniNichev <toni.nichev@gmail.com>
2020-04-26 20:08:35 -0400

    Push test

--- jenkins/pr.groovy
+++ jenkins/pr.groovy
@@ -1,7 +1,9 @@
+import hudson.security.csrf.DefaultCrumbIssuer
 import jenkins.model.Jenkins
+ 
 def instance = Jenkins.instance
-instance.setCrumbIssuer(null)
-
+instance.setCrumbIssuer(new DefaultCrumbIssuer(true))
+instance.save()
 
 pipeline {
   agent any
