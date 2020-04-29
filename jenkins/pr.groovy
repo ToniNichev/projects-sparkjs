f0f499c65e089c5c6c0df02124109c974044b121
ToniNichev <toni.nichev@gmail.com>
2020-04-28 22:49:52 -0400

    Adding .scannerwork to ignored folders

--- jenkins/pr.groovy
+++ jenkins/pr.groovy
@@ -1,3 +1,11 @@
+import hudson.security.csrf.DefaultCrumbIssuer
+import jenkins.model.Jenkins
+ 
+def instance = Jenkins.instance
+instance.setCrumbIssuer(new DefaultCrumbIssuer(true))
+instance.save()
+
+
 pipeline {
   agent any
     
