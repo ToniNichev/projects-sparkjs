285f473180f6ab879eed740e2ac16a4a48ca49ab
ToniNichev <toni.nichev@gmail.com>
2020-05-06 18:52:19 -0400

    Push test

--- jenkins/pr.groovy
+++ jenkins/pr.groovy
@@ -37,13 +37,13 @@ pipeline {
       }
     }
 
-    stage('Running SonarQube Scanner') {
+    stage('Deploy and run server') {
       steps { 
           echo 'Starting server ...'
           sh '/usr/local/bin/yarn clean; /usr/local/bin/yarn build-prod; /usr/local/bin/yarn build-prod-ssr;'
           sh '/usr/local/bin/pm2 start ./server-build/server-bundle.js -f'
       }
     }      
-    
+
   }
 }
