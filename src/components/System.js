6a5e13344731dae95600fb70e1ecbde821d5c8b5
ToniNichev <toni.nichev@gmail.com>
2020-07-14 23:51:00 -0400

    Work

---
+++ src/components/System.js
@@ -0,0 +1,16 @@
+import React, { Component, useEffect, useState } from 'react';
+
+
+
+const AboutContainer = () => {
+
+  return (
+    <div>    
+        <h2>{ process.env.APP_NAME } </h2>
+        config override
+    </div>
+  );
+
+}
+
+export default AboutContainer;
