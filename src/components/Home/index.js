20d49a8f5b3bfb12598fd7e2f682d8fe8c01ffd5
ToniNichev <toni.nichev@gmail.com>
2020-07-14 23:49:41 -0400

    work

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import styles from './styles.scss';
 import MockAPI from './__mocks__/api-data.json';
+import Loadable from 'react-loadable';
 
+
+import FeatureComponent from './FeatureComponent';
 
 const Renderer = ({title}) => {
   const namesArray = typeof global.__API_DATA__ !== 'undefined' ? global.__API_DATA__.home : MockAPI.home;
   const names = namesArray.map( (data) => { return data.first_name }).join(',');
 
-  const FeatureComponent = global.__API_DATA__.featureFlags.one ? (<div>Feature Flag</div>) : null;
+  //const FeatureComponent = global.__API_DATA__.featureFlags.one ? (<div>Feature Flag</div>) : null;
   return (
     <div className={styles.wrapper}>
         <div className={styles.leftRail}>
           <div className={styles.title}>{title}</div>
             <p>{names}</p>
-            {FeatureComponent}
+            {global.__API_DATA__.featureFlags.one ? FeatureComponent : null}
         </div>
         <div className={styles.rightRail}>
           <p>right rail</p>
